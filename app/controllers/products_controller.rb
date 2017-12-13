bd05bbf4aa13716df5ae93a76f8289b4e8b84b40
Parv Kumar <parvathikumar92@gmail.com>
2017-12-13 19:43:36 +0000

    automatically backed up by learn

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,12 @@
+class ProductsController < Application::Controller 
+
+  def index 
+    @products = Product.all 
+  end 
+
+  def add 
+    @product = Product.find(params[:id])
+    cart << @product
+  end 
+  
+end 
